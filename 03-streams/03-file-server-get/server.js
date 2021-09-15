357ea0ac27389f41548bbf4b725a4676b3b2c20e
Andrii Ilin <andrey.ilyin83@gmail.com>
2021-09-15 19:19:09 +0300

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -2,19 +2,25 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 
+const fileDownloadHandler = require('./handler');
+
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.indexOf('/') >= 0) {
+    res.statusCode = 400;
+    res.end();
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+      fileDownloadHandler(filepath, req, res);
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
