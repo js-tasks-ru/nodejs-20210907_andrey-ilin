cb37a2de2ee6135783ae9d4275fa13041b2f29af
Andrii Ilin <andrey.ilyin83@gmail.com>
2021-09-16 14:08:57 +0300

    fix 3-module 4-task test failed

--- 03-streams/04-file-server-post/handler.js
+++ 03-streams/04-file-server-post/handler.js
@@ -14,10 +14,9 @@ const fileUploadHandler = (filepath, req, res) => {
 
             limitStream.on('error', (err) => {
                 if (err.code === 'LIMIT_EXCEEDED') {
-                    fs.rm(filepath, { force: true }, () => {
-                        res.statusCode = 413;
-                        res.end()
-                    });
+                    fs.unlinkSync(filepath);
+                    res.statusCode = 413;
+                    res.end();
                 }
             })
 
@@ -27,9 +26,8 @@ const fileUploadHandler = (filepath, req, res) => {
             });
 
             req.on('aborted', () => {
-                fs.rm(filepath, { force: true }, () => {
-                    fileWriter.destroy();
-                });
+                fs.unlinkSync(filepath);
+                fileWriter.destroy();
             });
         } else {
             res.statusCode = 409;
