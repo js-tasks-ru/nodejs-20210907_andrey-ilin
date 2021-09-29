64015bfa5dc621f6052cb3b8bd654ae0ee5a858d
Andrii Ilin <andrey.ilyin83@gmail.com>
2021-09-29 16:29:09 +0300

    add solution module-5 task-1

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,11 +8,27 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let promiseResolversList = [];
+// not using, maybe redundant
+let promiseRejectsList = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    ctx.body = await new Promise((resolve, reject) => {
+        promiseResolversList.push((message) => resolve(message));
+        promiseRejectsList.push(reject);
+    });
 });
 
 router.post('/publish', async (ctx, next) => {
+    if (ctx.request.body.message) {
+        ctx.body = `message ${ctx.request.body.message} received`;
+        promiseResolversList.forEach((resolve) => {
+            resolve(ctx.request.body.message);
+        });
+        promiseResolversList = [];
+    }
 });
+
 
 app.use(router.routes());
 
