0bb319bf303dde8d065342dc2c6312134529dca8
Andrii Ilin <andrey.ilyin83@gmail.com>
2021-10-22 13:00:05 +0300

    add solution for 9-module 1-task

--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -6,12 +6,35 @@ const Message = require('./models/Message');
 function socket(server) {
   const io = socketIO(server);
 
-  io.use(async function(socket, next) {
-    next();
+  io.use(async function (socket, next) {
+    const token = socket.handshake.query.token;
+
+    if (!token) {
+      next(new Error('anonymous sessions are not allowed'));
+      return;
+    }
+
+    const session = await Session.findOne({ token }).populate('user');
+
+    if (session) {
+      socket.user = session.user;
+      next();
+    } else {
+      next(new Error('wrong or expired session token'));
+    }
   });
 
-  io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+  io.on('connection', function (socket) {
+    socket.on('message', async (msg) => {
+      const message = new Message({
+        date: new Date(),
+        text: msg,
+        chat: socket.user._id,
+        user: socket.user.displayName,
+      });
+
+      await message.save();
+    });
   });
 
   return io;
